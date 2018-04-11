1d4ccfcd703adc86f381492f25fb7894a175ab68
Daniel Lai <dlai813@gmail.com>
2018-04-10 17:06:30 -0700

    implemented initial file structure skeleton

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const routes = require('./routes/router');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(__dirname, '../client/dist'));
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+})
