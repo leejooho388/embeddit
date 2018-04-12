7fefe1a3d8e6420aee1a5d3d77372417929e1fb7
Daniel Lai <dlai813@gmail.com>
2018-04-12 08:58:31 -0700

    created exampleData for users, sr, and posts

--- client/src/components/content.jsx
+++ client/src/components/content.jsx
@@ -2,27 +2,59 @@ import React from 'react'
 import { Feed, Icon } from 'semantic-ui-react'
 
 const Content = () => (
-  <Feed>
-    <Feed.Event> 1
-      <Feed.Label>
-        {/* <img src='./../../upvoteArrow.png'/> */}
-      </Feed.Label>
-      <Feed.Content>
-        <Feed.Summary>
-          <Feed.Date>submitted 1 Hour Ago by </Feed.Date>
-          <Feed.User>Elliot Fu</Feed.User>
-        </Feed.Summary>
-        <Feed.Meta>
-          <Feed.Like>
-            
-            <Icon name='like' />
-            4 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
+  // <Feed>
+  //   <Feed.Event> 1
+  //     <Feed.Label>
+  //       {/* <img src='./../../upvoteArrow.png'/> */}
+  //     </Feed.Label>
+  //     <Feed.Content>
+  //       <Feed.Summary>
+  //         <Feed.Date>submitted 1 Hour Ago by </Feed.Date>
+  //         <Feed.User>Elliot Fu</Feed.User>
+  //       </Feed.Summary>
+  //       <Feed.Meta>
+  //         <Feed.Like>
+  //           4 Likes
+  //         </Feed.Like>
+  //       </Feed.Meta>
+  //     </Feed.Content>
+  //   </Feed.Event>
 
-  </Feed>
+  // </Feed>
+  <div class="ui celled grid">
+    <div class="row">
+      <div class="three wide column">
+        <img>
+      </div>
+    <div class="thirteen wide column">
+      <p></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="three wide column">
+      <img>
+    </div>
+    <div class="ten wide column">
+      <p></p>
+    </div>
+    <div class="three wide column">
+      <img>
+    </div>
+  </div>
+</div>
 )
 
 export default Content;
+
+/*
+post order number,
+
+upvote arrow(changes upon click),
+total vote count(changes upon click),
+downvote arrow(changes upon click),
+
+title(linked to either the post on reddit),
+submitted 'timestamp' ago by 'username' to 'sr'
+
+
+*/
---
+++ exampleData/exampleUsers.js
@@ -0,0 +1,34 @@
+const exampleUsers = [
+  {
+    username: 'zgallagher',
+    password: 'asdfj',
+    post_karma: 0,
+    comment_karma: 0,
+    sr_ids:[]
+  },
+
+  {
+    username: 'jlee',
+    password: 'asdfk',
+    post_karma: 0,
+    comment_karma: 0,
+    sr_ids:[]
+  },
+  
+  {
+    username: 'dlai',
+    password: 'asdfl',
+    post_karma: 0,
+    comment_karma: 0,
+    sr_ids:[]
+  },
+  
+  {
+    username: 'nsanchez',
+    password: 'asdf',
+    post_karma: 0,
+    comment_karma: 0,
+    sr_ids:[]
+  }
+  
+]
