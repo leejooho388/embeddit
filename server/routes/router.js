9f1f1e38ba383580468c2e037eef95e65893bc3a
Daniel Lai <dlai813@gmail.com>
2018-04-13 10:49:10 -0700

    refactored post related files

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const subredditController = require('../../db/controllers/subredditControl.js');
-const postController_get = require('../../db/controllers/postController_get')
+const postController = require('../../db/controllers/postController.js')
 
+// SUBREDDIT
 router.get('/subreddit', subredditController.getSubreddit);
 router.get('/subreddit/:query', subredditController.getQuerySubreddit);
 router.post('/subreddit', subredditController.postSubreddit);
-const newPost = require('../../db/controllers/postController.js');
-// send to /posts
-router.post('/post', newPost.newPost)
 
-router.get('/posts', postController_get.get);
+// POST
+router.get('/post', postController.get);
+router.post('/post', postController.newPost)
+
 
 module.exports = router;
