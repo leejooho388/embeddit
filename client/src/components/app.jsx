990066d0481f7188eb86d31e93d7eb4d6ef072a1
Nikko Sanchez <neeks08@gmail.com>
2018-04-12 19:31:40 -0700

    Rebased and fixed a bunch of previous errors, Now should be perfect

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -2,12 +2,7 @@ import React, { Component } from 'react';
 import Content from './content.jsx';
 import NavHead from './navHead.jsx';
 import Head2 from './head2.jsx';
-<<<<<<< 887a68468f4679929200bf35c2ad02edea8361a3
-
-import Post from './Post.jsx'
-=======
 import Post from './post.jsx';
->>>>>>> Fixed post form css width and saves to mongo properly
 export default class App extends Component {
   render() {
     return (
@@ -17,10 +12,6 @@ export default class App extends Component {
         <Head2 />
         {/* <Content /> */}
         <Post />
-<<<<<<< 887a68468f4679929200bf35c2ad02edea8361a3
-        {/*{this.props.children}*/}
-=======
->>>>>>> Fixed post form css width and saves to mongo properly
       </div>
     );
   }
