19bfb680a997826ac076416137e29ea49b2e4ecf
Daniel Lai <dlai813@gmail.com>
2018-04-13 17:13:18 -0700

    implemented post request from commentInput Box

--- client/src/components/commentInputBox.jsx
+++ client/src/components/commentInputBox.jsx
@@ -1,34 +1,47 @@
 import React, { Component } from "react";
 import { Form, Button } from 'semantic-ui-react';
+import axios from 'axios';
 
 class CommentInputBox extends Component {
   constructor(props) {
-    super(props),
+    super(props);
     this.state = {
-      // parentType: // post = 0, comment = 1
-      // parentId: //post or comment _id
-      text: ''
-      // voteCount: 0,
-      // author: {
-      //   authorId: //user._id
-      //   name: //username
+      parentType: 0,// post = 0, comment = 1
+      parentId: 'testing',//post or comment _id
+      text: '',
+      voteCount: 0,
+      author: {
+        authorId: '',//user._id
+        name: ''//username
       }
-      this.handleOnChange = this.handleOnChange.bind(this);
     }
+      this.handleOnChange = this.handleOnChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleOnChange(e) {
     this.setState({text: e.target.value});
-    console.log(this.state.text);
   }
 
+  handleSubmit() {
+    let newComment = Object.assign({}, this.state);
+    axios.post('http://localhost:8080/api/comments', newComment)
+    .then( res => {
+        this.setState({text: ''});
+        console.log('Success on comment posting')
+      })
+      .catch( err => {
+        console.log('Error on comment posting', err);
+      })
+  }
 
   render() {
     return(
       <Form id='commentInputBox'>
         <Form.Field width='7'>
           <div style={{color: 'rgb(182, 49, 52)'}}>Don't just complain, please MESSAGE THE MODS regarding rule violations</div>
-          <Form.TextArea rows='4' onChange={this.handleOnChange}/>
-          <Button basic size='mini' color='grey'>save</Button>
+          <Form.TextArea rows='4' onChange={this.handleOnChange} value={this.state.text}/>
+          <Button basic size='mini' color='grey' onClick={this.handleSubmit}>save</Button>
         </Form.Field>
       </Form>
     )
