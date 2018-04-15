f36f90d32ce0328c5a2d383cbc7aeacf94a995a2
Zach Gallagher <zachagallagher@gmail.com>
2018-04-14 23:33:28 -0700

    Got post, user, vote data into server & limited vote to authed users

--- db/controllers/voteController.js
+++ db/controllers/voteController.js
@@ -2,12 +2,23 @@ const Post = require('../models/Posts.js');
 
 const voteController = {
   post: (req, res) => {
-    console.log('POST req.body', req.body);
+
+    // req.body { postId: '5ad26cf9665cb8a24aa5770a',
+    //   userId: '5ad0387dc939da247cf9ed7b',
+    //   vote: 1 }
+
+
+
     res.status(200).end();
   },
 
-  delete: (req, res) => {
-    console.log('DELETE req.body', req.body);
+  put: (req, res) => {
+
+    // req.body { postId: '5ad26cf9665cb8a24aa5770a',
+    //   userId: '5ad0387dc939da247cf9ed7b' }
+
+
+
     res.status(200).end();
   }
 }
