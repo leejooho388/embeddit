c3e1c29db1c95ec8ccbe98bcc7b0ca661a5dfd9b
unknown <leejooho388@gmail.com>
2018-04-16 17:51:12 -0700

    added basic filtering but still has bugs

--- server/routes/router.js
+++ server/routes/router.js
@@ -35,6 +35,7 @@ router.post('/comments', commentController.post);
 // POST VOTES
 router.post('/r/:subreddit/:id/vote', postVoteController.post);
 router.put('/r/:subreddit/:id/vote', postVoteController.put);
+router.get('/r/:subreddit/', postController.getSubreddit)
 
 // COMMENT VOTES
 router.post('/comment/vote/:commentId', commentVoteController.post);
